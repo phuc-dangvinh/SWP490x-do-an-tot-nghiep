df4616a8afde78e934f88eaabb2e331dbb7b50ff
Dang Vinh Phuc <phuc.dangvinh@ncc.asia>
2023-06-12 16:25:27 +0700

    12.06.2023

--- FrontEnd/swp490x_frontend/src/app/app.module.ts
+++ FrontEnd/swp490x_frontend/src/app/app.module.ts
@@ -11,8 +11,6 @@ import { MenuBarComponent } from './components/share/menu-bar/menu-bar.component
 import { PageNotFoundComponent } from './components/share/page-not-found/page-not-found.component';
 import { AdminModule } from './components/admin/admin.module';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { NgIconsModule } from '@ng-icons/core';
-// import { heroHome } from '@ng-icons/heroicons/outline';
 
 @NgModule({
   declarations: [AppComponent, MenuBarComponent, PageNotFoundComponent],
@@ -20,13 +18,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    // MenubarModule,
     AdminModule,
     CartModule,
     AccountModule,
     HomeModule,
     NgbModule,
-    // NgIconsModule.withIcons({ heroHome }),
   ],
   providers: [],
   bootstrap: [AppComponent],
--- FrontEnd/swp490x_frontend/src/app/components/admin/admin.module.ts
+++ FrontEnd/swp490x_frontend/src/app/components/admin/admin.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { UserManagementComponent } from './user-management/user-management.component';
 import { ProductManagementComponent } from './product-management/product-management.component';
 import { CategoryManagementComponent } from './category-management/category-management.component';
+import { PagingComponent } from '../share/paging/paging.component';
 
 @NgModule({
   declarations: [
     UserManagementComponent,
     ProductManagementComponent,
     CategoryManagementComponent,
+    PagingComponent,
   ],
   imports: [CommonModule],
 })
