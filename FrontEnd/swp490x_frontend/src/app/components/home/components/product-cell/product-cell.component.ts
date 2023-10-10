b9aecc6d28f6705bd0fcf4d314a4cf00e99f4cd5
Dang Vinh Phuc <phuc.dangvinh@ncc.asia>
2023-10-10 16:50:16 +0700

    build home page

---
+++ FrontEnd/swp490x_frontend/src/app/components/home/components/product-cell/product-cell.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-cell',
+  templateUrl: './product-cell.component.html',
+  styleUrls: ['./product-cell.component.scss']
+})
+export class ProductCellComponent {
+
+}
