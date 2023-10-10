7c8212cd3b0589a256ed5976582ae8c3f379870b
Dang Vinh Phuc <phuc.dangvinh@ncc.asia>
2023-10-10 16:50:25 +0700

    build home page

--- FrontEnd/swp490x_frontend/src/app/components/home/home.module.ts
+++ FrontEnd/swp490x_frontend/src/app/components/home/home.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
+import { ProductCellComponent } from './components/product-cell/product-cell.component';
+import { ProductDetailComponent } from './components/product-detail/product-detail.component';
+import { ShareModule } from '../share/share.module';
 
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ProductCellComponent,
+    ProductDetailComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ShareModule
   ]
 })
 export class HomeModule { }
